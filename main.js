f73819fc8c58359e84c33d27b33daa54e74f7a7f
swoniix <sonyabl909gmail.com>
2026-04-29 15:44:00 +0300

    commit

--- main.js
+++ main.js
@@ -44,3 +44,78 @@ console.log(M.solve());
 //2
 class Shape {
 }
+class Square extends Shape {
+    x;
+    y;
+    size;
+    constructor(x, y, size) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    show() {
+        console.log(`Square: (${this.x},${this.y}), side ${this.size}`);
+    }
+    save() {
+        return `(${this.x},${this.y}),${this.size}`;
+    }
+    load(data) {
+        const parts = data.split(' ');
+        this.x = Number(parts[0]);
+        this.y = Number(parts[1]);
+        this.size = Number(parts[2]);
+        console.log('Square data has been loaded!');
+    }
+}
+class Rectangle extends Shape {
+    x;
+    y;
+    height;
+    width;
+    constructor(x, y, height, width) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+    }
+    show() {
+        console.log(`Rectangle: (${this.x},${this.y}), Height: ${this.height}, Width: ${this.width} `);
+    }
+    save() {
+        return `(${this.x},${this.y}),${this.height},${this.width}`;
+    }
+    load(data) {
+        const parts = data.split(' ');
+        this.x = Number(parts[0]);
+        this.y = Number(parts[1]);
+        this.height = Number(parts[2]);
+        this.width = Number(parts[3]);
+        console.log('Rectangle data has been loaded!');
+    }
+}
+class Circule extends Shape {
+    x;
+    y;
+    radius;
+    constructor(x, y, radius) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    show() {
+        console.log(`Rectangle: (${this.x},${this.y}), Radius ${this.radius} `);
+    }
+    save() {
+        return `(${this.x},${this.y}),${this.radius}`;
+    }
+    load(data) {
+        const parts = data.split(' ');
+        this.x = Number(parts[0]);
+        this.y = Number(parts[1]);
+        this.radius = Number(parts[2]);
+        console.log('Rectangle data has been loaded!');
+    }
+}
--- main.ts
+++ main.ts
@@ -34,10 +34,81 @@ const N = new LinearEquation(2,4)
 console.log(N.solve())
 const M = new QuadraticEquation(1,-3,2)
 console.log(M.solve())
+
+
 //2
 abstract class Shape
 {
     abstract show() : void
     abstract save(): string
     abstract load(data:string): void
+}
+class Square extends Shape{
+    constructor(private x: number, private y:number, private size:number)
+    {
+        super();
+    }
+    show(): void
+    {
+        console.log(`Square: (${this.x},${this.y}), side ${this.size}`)
+    }
+    save():string
+    {
+        return `(${this.x},${this.y}),${this.size}`
+    }
+    load(data: string): void
+    {
+        const parts = data.split(' ')
+        this.x = Number(parts[0])
+        this.y = Number(parts[1])
+        this.size = Number(parts[2])
+        console.log('Square data has been loaded!')
+    }
+}
+ class Rectangle extends Shape
+    {
+    constructor(private x: number, private y:number,private height: number,private width: number)
+    {
+        super();
+    }
+    show(): void
+    {
+        console.log(`Rectangle: (${this.x},${this.y}), Height: ${this.height}, Width: ${this.width} `)
+    }
+    save():string
+    {
+        return `(${this.x},${this.y}),${this.height},${this.width}`
+    }
+    load(data: string): void
+    {
+        const parts = data.split(' ')
+        this.x = Number(parts[0])
+        this.y = Number(parts[1])
+        this.height = Number(parts[2])
+        this.width = Number(parts[3])
+        console.log('Rectangle data has been loaded!')
+    }
+}
+ class Circule extends Shape
+    {
+    constructor(private x: number, private y:number,private radius: number)
+    {
+        super();
+    }
+    show(): void
+    {
+        console.log(`Rectangle: (${this.x},${this.y}), Radius ${this.radius} `)
+    }
+    save():string
+    {
+        return `(${this.x},${this.y}),${this.radius}`
+    }
+    load(data: string): void
+    {
+        const parts = data.split(' ')
+        this.x = Number(parts[0])
+        this.y = Number(parts[1])
+        this.radius = Number(parts[2])
+        console.log('Rectangle data has been loaded!')
+    }
 }
